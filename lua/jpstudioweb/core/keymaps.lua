a07e3456c32e242b4a6edd7a0478b2c941b6c332
jpstudioweb <jpaulo6ba@gmail.com>
2023-10-12 08:29:39 -0300

    add "" quotes in files html

--- lua/jpstudioweb/core/keymaps.lua
+++ lua/jpstudioweb/core/keymaps.lua
@@ -33,3 +33,24 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Automatically adding quotes when adding an attribute
+keymap.set("i", "=", function()
+  -- The cursor location does not give us the correct node in this case, so we
+  -- need to get the node to the left of the cursor
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+
+  local node = vim.treesitter.get_node({ pos = left_of_cursor_range })
+  local nodes_active_in = {
+    "attribute_name",
+    "directive_argument",
+    "directive_name",
+  }
+  if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+    -- The cursor is not on an attribute node
+    return "="
+  end
+
+  return '=""<left>'
+end, { expr = true, buffer = true })
