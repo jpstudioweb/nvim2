0eca5bd7fe5def1a72cfe9b39afa42b93dcfbf3f
jpstudioweb <jpaulo6ba@gmail.com>
2024-01-17 10:39:05 -0300

    change keymap from execute ruby to execute python

--- lua/jpstudioweb/core/keymaps.lua
+++ lua/jpstudioweb/core/keymaps.lua
@@ -12,8 +12,8 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- execute ruby code
-keymap.set("n", "<leader>b", ":! ruby %<CR>")
+-- execute python code
+keymap.set("n", "<leader>b", ":! python %<CR>")
 
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
