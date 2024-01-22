83546e4f80b0b15713fb1d21fc8857ba256c0f69
jpstudioweb <jpaulo6ba@gmail.com>
2024-01-22 18:36:28 -0300

    add description to keys

--- lua/jpstudioweb/plugins/which-key.lua
+++ lua/jpstudioweb/plugins/which-key.lua
@@ -1,6 +1,20 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  config = function()
+    -- Configurações do plugin which-key
+    require("which-key").setup({
+      -- Sua configuração personalizada vem aqui
+    })
+    -- Registrar atalhos de teclado e suas descrições
+    require("which-key").register({
+      ["<leader>"] = {
+        c = "Descrição do comando c",
+        d = "Descrição do comando d",
+        -- Adicione descrições adicionais para outras teclas aqui
+      },
+    })
+  end,
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
