9a725e1ab17fa2aab1225b73cef3cf4344f4ff6b
jpstudioweb <jpaulo6ba@gmail.com>
2024-01-24 15:45:45 -0300

    remove configs noice plugin

--- lua/jpstudioweb/plugins/noice.lua
+++ lua/jpstudioweb/plugins/noice.lua
@@ -2,34 +2,10 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-      cmdline = {
-        view = "cmdline_popup",
-        opts = { buf_options = { filetype = "vim" } },
-        filter_options = {
-          auto_open = true,
-        },
-      },
-      messages = {
-        enabled = true,
-        view = "notify",
-        view_error = "notify",
-        view_warn = "notify",
-        view_history = "messages",
-        view_search = "virtualtext",
-      },
-      popupmenu = {
-        enabled = true,
-        backend = "nui",
-      },
-      redirect = {
-        view = "popup",
-        filter = { event = "msg_show" },
-      },
-    },
+    opts = {},
     dependencies = {
       "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     },
   },
 }
