7bf92fe48971e4111ac0812990d333a24af8c5e9
jpstudioweb <jpaulo6ba@gmail.com>
2024-01-25 09:25:12 -0300

    config noice

--- lua/jpstudioweb/plugins/noice.lua
+++ lua/jpstudioweb/plugins/noice.lua
@@ -10,22 +10,7 @@ return {
     config = function()
       local noice = require("noice")
       noice.setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
         -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
         cmdline = {
           enable = true,
           view = "cmdline_popup",
@@ -62,19 +47,16 @@ return {
           enabled = true, -- enables the Noice popupmenu UI
           ---@type 'nui'|'cmp'
           backend = "nui", -- backend to use to show regular cmdline completions
-          ---@type NoicePopupmenuItemKind|false
           -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
           kind_icons = {}, -- set to `false` to disable icons
         },
         -- default options for require('noice').redirect
         -- see the section on Command Redirection
-        ---@type NoiceRouteConfig
         redirect = {
           view = "popup",
           filter = { event = "msg_show" },
         },
         -- You can add any custom commands below that will be available with `:Noice command`
-        ---@type table<string, NoiceCommand>
         commands = {
           history = {
             -- options for the message history that you get with `:Noice`
@@ -128,26 +110,23 @@ return {
             enabled = true,
             -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
             -- See the section on formatting for more details on how to customize.
-            --- @type NoiceFormat|string
             format = "lsp_progress",
-            --- @type NoiceFormat|string
             format_done = "lsp_progress_done",
             throttle = 1000 / 30, -- frequency to update lsp progress message
             view = "mini",
           },
           override = {
             -- override the default lsp markdown formatter with Noice
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             -- override the lsp markdown formatter with Noice
-            ["vim.lsp.util.stylize_markdown"] = false,
+            ["vim.lsp.util.stylize_markdown"] = true,
             -- override cmp documentation with Noice (needs the other options to work)
-            ["cmp.entry.get_documentation"] = false,
+            ["cmp.entry.get_documentation"] = true,
           },
           hover = {
             enabled = true,
             silent = false, -- set to true to not show a message if hover is not available
             view = nil, -- when nil, use defaults from documentation
-            ---@type NoiceViewOptions
             opts = {}, -- merged with defaults from documentation
           },
           signature = {
@@ -159,7 +138,6 @@ return {
               throttle = 50, -- Debounce lsp signature help request by 50ms
             },
             view = nil, -- when nil, use defaults from documentation
-            ---@type NoiceViewOptions
             opts = {}, -- merged with defaults from documentation
           },
           message = {
@@ -171,7 +149,6 @@ return {
           -- defaults for hover and signature help
           documentation = {
             view = "hover",
-            ---@type NoiceViewOptions
             opts = {
               lang = "markdown",
               replace = true,
@@ -204,7 +181,6 @@ return {
           -- add any filetypes here, that shouldn't trigger smart move.
           excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
         },
-        ---@type NoicePresets
         presets = {
           -- you can enable a preset by setting it to true, or a table that will override the preset config
           -- you can also add custom presets that you can enable/disable with enabled=true
