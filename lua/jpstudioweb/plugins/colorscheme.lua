76511813fecb2edc0a546420a489aae64a8df5da
jpstudioweb <jpaulo6ba@gmail.com>
2024-06-06 14:14:03 -0300

    add theme tokyonight

--- lua/jpstudioweb/plugins/colorscheme.lua
+++ lua/jpstudioweb/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "kdheepak/monochrome.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme monochrome]])
+      vim.cmd([[colorscheme tokyonight]])
     end,
   },
 }
